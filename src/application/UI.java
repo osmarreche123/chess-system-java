87e2c30e20618b0f50e665e9ebe68f8f9b7524c3
Osmar Reche Junior <osmar_reche@hotmail.com>
2021-03-24 21:46:33 -0300

    Check mate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,10 +57,17 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()){
-            System.out.println("CHECK!!");
+        if(!chessMatch.getCheckMate()){
+            System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()){
+                System.out.println("CHECK!!");
+            }
         }
+        else {
+            System.out.println("CHECK MATE!");
+            System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
+        }
+
     }
 
     public static void printBoard(ChessPiece[][] pieces){
