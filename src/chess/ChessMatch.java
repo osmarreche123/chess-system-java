c153e0632bc3850b71544eeb88fe255829b43910
Osmar Reche Junior <osmar_reche@hotmail.com>
2021-03-27 23:23:57 -0300

    Update #promotion pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,7 +6,7 @@ import boardgame.Position;
 import chess.pieces.*;
 
 import java.lang.annotation.Target;
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -116,7 +116,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to promoted");
         }
         if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
